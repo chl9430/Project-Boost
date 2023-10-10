746063bef16437d83d818c66f04a73dfc9f0ff0d
Chris <chl9430@gmail.com>
2023-10-10 21:25:39 +0900

    Added Under scene. Delaying going to the next level using Invoke method.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float levelLoadDelay;
+
     void OnCollisionEnter(Collision collision)
     {
         switch(collision.gameObject.tag)
@@ -10,14 +13,41 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("Friendly");
                 break;
             case "Finish":
-                Debug.Log("Congratulation");
-                break;
-            case "Fuel":
-                Debug.Log("Fuel");
+                StartSuccessSeqence();
                 break;
             default:
-                Debug.Log("A!");
+                StartCrashSequence();
                 break;
         }
+    }
+
+    void StartSuccessSeqence()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    void StartCrashSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if(nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
