242ec7a70bffe67d5dde4b18adfcffcd585eb6c6
Chris <chl9430@gmail.com>
2023-10-10 18:50:35 +0900

    Controlling playing audioclip when we want.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,10 +7,13 @@ public class Movement : MonoBehaviour
     [SerializeField] float mainThrust;
     [SerializeField] float rotationThrust;
     Rigidbody rb;
+    AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -25,6 +28,14 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            if(!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
     }
 
